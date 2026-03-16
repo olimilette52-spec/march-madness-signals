dc33c2c6c8e4fe892f2c9bc18e248d9c0e7fb82c
olimilette52-spec <oli_milette52@hotmail.com>
2026-03-16 15:46:07 -0400

    Refactor team exports and update matchups format

--- src/data/teams.js
+++ src/data/teams.js
@@ -1,3 +1,8 @@
+export { TEAMS_EAST as _e } from './teamsEast'
+export { TEAMS_WEST as _w } from './teamsWest'
+export { TEAMS_SOUTH as _s } from './teamsSouth'
+export { TEAMS_MIDWEST as _m } from './teamsMidwest'
+
 import { TEAMS_EAST } from './teamsEast'
 import { TEAMS_WEST } from './teamsWest'
 import { TEAMS_SOUTH } from './teamsSouth'
@@ -13,7 +18,6 @@ export const TEAMS = {
 export const TEAMS_LIST = Object.values(TEAMS)
 
 export const MATCHUPS = [
-  // EAST
   { id: 1,  region: 'East',    home: 'DUKE',  away: 'SIENA', time: 'Jeu 19 Mar 19:10' },
   { id: 2,  region: 'East',    home: 'OSU',   away: 'TCU',   time: 'Jeu 19 Mar 12:15' },
   { id: 3,  region: 'East',    home: 'SJU',   away: 'NIA',   time: 'Jeu 19 Mar 14:45' },
@@ -22,7 +26,6 @@ export const MATCHUPS = [
   { id: 6,  region: 'East',    home: 'MIST',  away: 'NDST',  time: 'Ven 20 Mar 12:15' },
   { id: 7,  region: 'East',    home: 'UCLA',  away: 'UCF',   time: 'Ven 20 Mar 14:45' },
   { id: 8,  region: 'East',    home: 'CONN',  away: 'FUR',   time: 'Ven 20 Mar 21:10' },
-  // WEST
   { id: 9,  region: 'West',    home: 'ARIZ',  away: 'LIU',   time: 'Ven 20 Mar 13:35' },
   { id: 10, region: 'West',    home: 'VILL',  away: 'USU',   time: 'Ven 20 Mar 16:10' },
   { id: 11, region: 'West',    home: 'WIS',   away: 'HPT',   time: 'Jeu 19 Mar 13:50' },
@@ -31,7 +34,6 @@ export const MATCHUPS = [
   { id: 14, region: 'West',    home: 'GONZ',  away: 'KNST',  time: 'Jeu 19 Mar 22:00' },
   { id: 15, region: 'West',    home: 'MIA',   away: 'MIZZ',  time: 'Ven 20 Mar 22:10' },
   { id: 16, region: 'West',    home: 'PUR',   away: 'QUNS',  time: 'Ven 20 Mar 19:35' },
-  // SOUTH
   { id: 17, region: 'South',   home: 'FLA',   away: 'LEH',   time: 'Jeu 19 Mar 13:15' },
   { id: 18, region: 'South',   home: 'CLEM',  away: 'IOWA',  time: 'Jeu 19 Mar 15:45' },
   { id: 19, region: 'South',   home: 'VAN',   away: 'MCNS',  time: 'Jeu 19 Mar 18:15' },
@@ -40,7 +42,6 @@ export const MATCHUPS = [
   { id: 22, region: 'South',   home: 'ILL',   away: 'PENN',  time: 'Ven 20 Mar 18:55' },
   { id: 23, region: 'South',   home: 'SMC',   away: 'TXAM',  time: 'Ven 20 Mar 21:25' },
   { id: 24, region: 'South',   home: 'HOU',   away: 'IDA',   time: 'Ven 20 Mar 13:50' },
-  // MIDWEST
   { id: 25, region: 'Midwest', home: 'MICH',  away: 'MIOH',  time: 'Jeu 19 Mar 19:10' },
   { id: 26, region: 'Midwest', home: 'GEO',   away: 'SLU',   time: 'Jeu 19 Mar 21:45' },
   { id: 27, region: 'Midwest', home: 'TTU',   away: 'AKR',   time: 'Ven 20 Mar 12:40' },
